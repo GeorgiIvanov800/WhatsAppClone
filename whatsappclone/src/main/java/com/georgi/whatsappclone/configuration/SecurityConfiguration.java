5476c48db677432fdb284f4ceeccd98ea839baf1
georgiivanov <gogata92@pm.me>
2025-01-23 23:26:58 +0100

    Add Custom JwtConverter to extract the roles from Keycloak

---
+++ whatsappclone/src/main/java/com/georgi/whatsappclone/configuration/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.georgi.whatsappclone.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "swagger-ui.html",
+                                        "/ws/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(auth ->
+                        auth.jwt(token ->
+                                token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
+
+        return http.build();
+    }
+}
