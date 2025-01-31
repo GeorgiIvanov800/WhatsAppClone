7d1f534d32384f35bf539c57843dcd3155b94138
georgiivanov <gogata92@pm.me>
2025-01-31 10:33:09 +0100

    create chat

--- whatsappclone/whatsapp-clone-ui/src/app/components/chat-list/chat-list.component.ts
+++ whatsappclone/whatsapp-clone-ui/src/app/components/chat-list/chat-list.component.ts
@@ -2,6 +2,8 @@ import {Component, input, InputSignal, output} from '@angular/core';
 import {ChatResponse, UserResponse} from '../../services/models';
 import {DatePipe} from '@angular/common';
 import {UserService} from '../../services/services/user.service';
+import {ChatService} from '../../services/services/chat.service';
+import {KeycloakService} from '../../utils/keycloak/keycloak.service';
 
 @Component({
   selector: 'app-chat-list',
@@ -20,7 +22,9 @@ export class ChatListComponent {
 
   chatSelected = output<ChatResponse>();
 
-  constructor(private userService:UserService) {
+  constructor(private userService:UserService,
+              private chatService: ChatService,
+              private keycloakService: KeycloakService) {
   }
 
   searchContact() {
@@ -45,6 +49,23 @@ export class ChatListComponent {
   }
 
   selectContact(contact: UserResponse) {
-
+      this.chatService.createChat({
+        'sender-id': this.keycloakService.userId as string,
+        'receiver-id': contact.id as string,
+      }).subscribe({
+        next: (res => {
+          const chat: ChatResponse = {
+            id: res.response,
+            name: contact.firstName + ' ' + contact.lastName,
+            receiverOnline: contact.online,
+            lastMessageTime: contact.lastSeen,
+            senderId: this.keycloakService.userId,
+            receiverId: contact.id
+          };
+          this.chats().unshift(chat);
+          this.searchNewContact = false;
+          this.chatSelected.emit(chat);
+        })
+      })
   }
 }
--- whatsappclone/whatsapp-clone-ui/src/app/pages/main/main.component.ts
+++ whatsappclone/whatsapp-clone-ui/src/app/pages/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatListComponent } from '../../components/chat-list/chat-list.component';
-import {ChatResponse, MessageResponse} from '../../services/models';
+import {ChatResponse, MessageRequest, MessageResponse} from '../../services/models';
 import {ChatService, MassageService} from '../../services/services';
 import {KeycloakService} from '../../utils/keycloak/keycloak.service';
 import {DatePipe} from '@angular/common';
 import {PickerComponent} from '@ctrl/ngx-emoji-mart';
 import {FormsModule} from '@angular/forms';
+import {EmojiData} from '@ctrl/ngx-emoji-mart/ngx-emoji';
 
 @Component({
   selector: 'app-main',
@@ -49,7 +50,7 @@ export class MainComponent implements OnInit {
       this.selectedChat = chatResponse;
       this.getAllChatMessages(chatResponse.id as string);
       this.setMessagesToSeen();
-      // this.selectedChat.unreadCount = 0;
+      this.selectedChat.unreadCount = 0;
   }
 
   private getAllChatMessages(chatId: string) {
@@ -63,7 +64,11 @@ export class MainComponent implements OnInit {
   }
 
   private setMessagesToSeen() {
-
+      this.messageService.setMessagesToSeen({
+        'chat-id': this.selectedChat.id as string
+      }).subscribe({
+        next: () => {}
+      });
   }
   isSelfMessage(message: MessageResponse) {
       return message.senderId === this.keyCloakService.userId;
@@ -74,18 +79,62 @@ export class MainComponent implements OnInit {
   }
 
   onSelectEmojis(emojiSelected: any) {
-
+        const emoji:EmojiData = emojiSelected.emoji;
+        this.messageContent += emoji.native;
   }
 
   keyDown(event: KeyboardEvent) {
-
+      if (event.key === 'Enter') {
+        this.sendMessage();
+      }
   }
 
   onClick() {
-
+    this.setMessagesToSeen();
   }
 
   sendMessage() {
+      if (this.messageContent) {
+        const messageRequest: MessageRequest = {
+          chatId: this.selectedChat.id,
+          senderId: this.getSenderId(),
+          receiverId: this.getReceiverId(),
+          content: this.messageContent,
+          type: 'TEXT'
+        };
+        this.messageService.saveMessage({
+          body: messageRequest
+        }).subscribe({
+          next: () => {
+           const message: MessageResponse = {
+             senderId: this.getSenderId(),
+             receiverId: this.getReceiverId(),
+             content: this.messageContent,
+             type: 'TEXT',
+             state: 'SENT',
+             createdAt: new Date().toString()
+           };
+           this.selectedChat.lastMessage = this.messageContent;
+           this.chatMessages.push(message);
+           this.messageContent = '';
+           this.showEmojis = false;
+          }
+        });
+      }
+  }
 
+  private getSenderId(): string {
+    if (this.selectedChat.senderId === this.keyCloakService.userId) {
+      return this.selectedChat.senderId as string;
+    }
+    return this.selectedChat.receiverId as string;
+
+  }
+
+  private getReceiverId():string {
+    if (this.selectedChat.senderId === this.keyCloakService.userId) {
+      return this.selectedChat.receiverId as string;
+    }
+    return this.selectedChat.senderId as string;
   }
 }
